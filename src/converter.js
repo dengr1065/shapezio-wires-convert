e4c8a99c14eb26270e4d70a6e3b8f832ac31d9f2
dengr1065 <dengr1065@gmail.com>
2020-08-13 23:21:56 +0300

    commit part of migration

--- src/converter.js
+++ src/converter.js
@@ -1,6 +1,8 @@
 const { compressToEncodedURIComponent, decompressFromEncodedURIComponent } = require("lz-string");
-const Rusha = require("rusha");
+const crc32 = require("crc/crc32").default;
+const spriteToCode = require("./building_codes");
 
+const CRC_PREFIX = "32".padEnd(32, "-");
 class DecompressError extends Error { }
 
 /**
@@ -21,30 +23,54 @@ function convertSavegame(save) {
         throw new DecompressError("Could not decompress savegame.");
     }
 
-    const hash = rawData.substr(0, 40);
-    const json = rawData.substr(40);
+    const json = rawData.startsWith(CRC_PREFIX) ?
+        rawData.substr(8) :
+        rawData.substr(40);
 
-    const hashBuilder = Rusha.createHash();
-    hashBuilder.update(json + salt);
-    const actualHash = hashBuilder.digest("hex");
-
-    if (hash != actualHash) {
-        throw new DecompressError("Hash mismatch.");
-    }
-
+    crc32()
     const obj = JSON.parse(json);
     const savegame = decompressObject(obj);
 
     savegame.dump.entities.forEach(entity => {
+        entity.components.Unremovable = undefined;
+        entity.components.Hub = undefined;
+
+        if (entity.components.StaticMapEntity) {
+            const mapEntity = entity.components.StaticMapEntity;
+            mapEntity.code = spriteToCode[mapEntity.spriteKey];
+
+            mapEntity.tileSize = undefined;
+            mapEntity.spriteKey = undefined;
+            mapEntity.blueprintSpriteKey = undefined;
+            mapEntity.silhouetteColor = undefined;
+        }
+
+        if (entity.components.Belt) {
+            entity.components.ItemAcceptor = undefined;
+            entity.components.ItemEjector = undefined;
+        }
+
         if (entity.components.ItemAcceptor) {
             entity.components.ItemAcceptor.slots.forEach(slot => {
                 slot.layer = "regular";
             });
-            entity.components.ItemAcceptor.beltUnderlays.forEach(underlay => {
-                underlay.layer = "regular";
-            })
+
+            if (entity.components.ItemAcceptor.beltUnderlays.length > 0) {
+                const underlays = entity.components.ItemAcceptor.beltUnderlays;
+                entity.components.ItemAcceptor.animated = undefined;
+                entity.components.ItemAcceptor.beltUnderlays = undefined;
+
+                entity.components.BeltUnderlays = {
+                    underlays: underlays.map(u => {
+                        u.layer = undefined;
+                        return u;
+                    })
+                };
+            }
         }
         if (entity.components.ItemEjector) {
+            entity.components.ItemEjector.instantEject = undefined;
+
             entity.components.ItemEjector.slots.forEach(slot => {
                 slot.layer = "regular";
             });
